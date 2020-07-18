154968e852f3da72fd448cd4aff3e6eee7447b20
sonpen <sonpen76@naver.com>
2020-07-18 22:40:43 +0900

    LongestSubstringWithoutRepeatingCharacters - Approach 2: sliding window - my version

--- src/main/java/com/redson/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/redson/LongestSubstringWithoutRepeatingCharacters.java
@@ -7,25 +7,17 @@ import java.util.Set;
  * Created by sonpen on 20. 7. 18.
  */
 public class LongestSubstringWithoutRepeatingCharacters {
-    private boolean hasRepeatCharacters(String s) {
-        Set<Character> charSet = new HashSet<>();
-        for(int i = 0; i < s.length(); i++ ) {
-            if(charSet.contains(s.charAt(i)) == true )
-                return true;
-            else
-                charSet.add(s.charAt(i));
-        }
-        return false;
-    }
-
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
         for( int i = 0; i < s.length(); i++) {
-            for( int j = i+1; j <= s.length(); j++) {
-                String substr = s.substring(i, j);
-                if( hasRepeatCharacters(substr) == false && maxLength < substr.length())
-                    maxLength = substr.length();
+            Set<Character> strSet = new HashSet<>();
+            for( int j = i; j < s.length(); j++) {
+                if( strSet.contains(s.charAt(j)) == true )
+                    break;
+                else
+                    strSet.add(s.charAt(j));
             }
+            maxLength = Math.max(maxLength, strSet.size());
         }
 
         return maxLength;
